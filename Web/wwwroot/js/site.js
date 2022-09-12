bed6b0b027e2b0ea81991c038f23dab34d165cf9
Alan-Nyled <alannyled@gmail.com>
2022-09-12 12:06:27 +0200

    Flyttet js, tilføjet en joker

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let btn = document.getElementById("draw-btn")
+let text = document.getElementById("result")
+let img = document.getElementById("img")
 
-// Write your JavaScript code.
+const getCard = () => {
+    fetch("https://localhost:7164/")
+        .then(response => response.json())
+        .then(result => {
+            let value
+            translate[result.value] ? value = translate[result.value] : value = [result.value]
+            text.innerText = `Du trak ${translate[result.type]} ${value}`
+            img.src = `images/${value}-${translate[result.type]}.jpg`
+        })
+        .catch(error => console.error('Det gik ikke: ', error));
+}
+
+const translate = {
+    Diamonds: "Ruder",
+    Hearts: "Hjerter",
+    Clubs: "Klør",
+    Spades: "Spar",
+    Joker: "En Joker",
+    Queen: "Dame",
+    King: "Konge",
+    Jack: "Knægt",
+    Ace: "Es"
+}
+
+btn.onclick = getCard
